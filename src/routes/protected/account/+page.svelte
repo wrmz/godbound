a0fe46dd0a176022afd6e265b0981d8831c66069
William Metz <wrmetzjr@gmail.com>
2022-10-30 21:01:46 -0400

    Unnecessary  files

--- src/routes/protected/account/+page.svelte
+++ src/routes/protected/account/+page.svelte
@@ -46,16 +46,6 @@
             <Button priority='low'>Delete My Account</Button>
         </div>
     </div>
-
-
-    <h1>Account</h1>
-    <p>Here the user can do the following:</p>
-    <ul>
-        <li>Change Global Email</li>
-        <li>Change Global Password</li>
-        <li>Change Global Privacy</li>
-        <li>Delete Account</li>
-    </ul>
 </section>
 
 <style>
